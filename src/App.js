2e9702e5112722dfe782201d3bd62d2bee48157b
Glagny <nikoicardo@gmail.com>
2021-09-21 15:52:58 -0600

    Added an alert message above the search bar that triggers when a search was submitted with no entry.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
-
 import './App.css';
 
 class App extends Component {
-  state = { users: [], loading: false };
+  state = { users: [], loading: false, alert: null };
 
   //Search Github Users
   searchUsers = async (text) => {
@@ -23,17 +23,26 @@ class App extends Component {
   //Clear Users from State
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
