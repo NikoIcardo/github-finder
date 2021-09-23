1dc1974cff01b8c9ad8a2d19703c1028bd4a3f78
Glagny <nikoicardo@gmail.com>
2021-09-23 17:56:01 -0600

    Added The Home and Not Found Pages and also cleaned up some files.

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p className="lead">
+        The page you have attempted to reach does not exist. Please check the
+        url.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
